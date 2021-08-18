c80a9a94144367271f920452725b02d5265ed295
salar <s.zardouz@fidilio.com>
2021-08-18 17:41:26 +0430

    add snail

---
+++ Assets/Helpers/Tags.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Helpers
+{
+   public class Tags
+    {
+
+        public const string PlayerTag = "Player";
+
+        public const string BeetleTag = "Beetle";
+    }
+}
--- Assets/Scripts/SnailScript.cs
+++ Assets/Scripts/SnailScript.cs
@@ -1,4 +1,6 @@
 using Assets.Enums;
+using Assets.Helpers;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,41 +13,123 @@ public class SnailScript : MonoBehaviour
     private Rigidbody2D body;
     private Animator animator;
     public float Speed = 1f;
-
-    public  Transform down_collision;
+    private bool stunned;
+    private bool canMove;    
+    public  Transform down_collision,left_collision,right_collision,top_collision;
+    public LayerMask playerLayer;
+    private Vector3 left_position, right_position;
 
     private bool moveLeft;
      void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        left_position = left_collision.position;
+        right_position = right_collision.position;
     }
     void Start()
     {
         moveLeft = true;
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (moveLeft)
+        if (canMove)
         {
-            body.velocity = new Vector2(-Speed, body.velocity.y);
-        }
-        else
-        {
-            body.velocity = new Vector2(Speed, body.velocity.y);
+            if (moveLeft)
+            {
+                body.velocity = new Vector2(-Speed, body.velocity.y);
+            }
+            else
+            {
+                body.velocity = new Vector2(Speed, body.velocity.y);
 
+            }
         }
+   
         CheckColision();
     }
 
     private void CheckColision()
     {
-        if (!Physics2D.Raycast(down_collision.position,Vector2.down,0.1f))
+
+        var leftHit = Physics2D.Raycast(left_collision.position, Vector2.left, 0.1f, playerLayer);
+        var rightHit = Physics2D.Raycast(right_collision.position, Vector2.right, 0.1f, playerLayer);
+        var topHit = Physics2D.OverlapCircle(top_collision.position, 0.2f, playerLayer);
+
+        if (topHit != null)
+        {
+            if (topHit.gameObject.CompareTag(Tags.PlayerTag))
+            {
+                if (!stunned)
+                {
+                    topHit.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
+                    stunned = true;
+                    canMove = false;
+                    body.velocity= new Vector2(0,0);
+
+                    if (tag == Tags.BeetleTag)
+                    {
+                        StartCoroutine(Dead(0.5f));
+                    }
+                    animator.Play("Stuned");
+
+                }
+            }
+        }
+
+        if (leftHit)
+        {
+            if (leftHit.collider.gameObject.CompareTag(Tags.PlayerTag))
+            {
+                if (!stunned)
+                {
+                    print("kirtoosh");
+                }
+                else
+                {
+
+                    if (tag != Tags.BeetleTag)
+                    {
+                        body.velocity = new Vector2(-15f, body.velocity.y);
+                        StartCoroutine(Dead(3f));
+                    }
+                }
+            }
+        }
+        if (rightHit)
+        {
+            if (rightHit.collider.gameObject.CompareTag(Tags.PlayerTag))
+            {
+                if (!stunned)
+                {
+                    print("kirtoosh");
+                }
+                else
+                {
+                    if (tag != Tags.BeetleTag)
+                    {
+                        body.velocity = new Vector2(15f, body.velocity.y);
+                        StartCoroutine(Dead(3f));
+                    }
+                }
+            }
+        }
+        if (!Physics2D.Raycast(down_collision.position,Vector2.down,0.01f))
         {
             ChangeDirection();
         }
+    }
+
+    private IEnumerator Dead(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        gameObject.SetActive(false);
+
     }
 
     private void ChangeDirection()
@@ -56,10 +140,16 @@ public class SnailScript : MonoBehaviour
 
         if (moveLeft)
         {
+
+            left_position = left_collision.position;
+            right_position = right_collision.position;
+
             temp.x = Mathf.Abs(temp.x);
         }
         else
         {
+            left_position = right_collision.position;
+            right_position= left_collision.position;
             temp.x = -Mathf.Abs(temp.x);
         }
 
